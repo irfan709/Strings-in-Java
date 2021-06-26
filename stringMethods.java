779ef720979ebc43fb851e640189f9345e942d73
irfan <73518373+irfan709@users.noreply.github.com>
2021-06-26 12:11:13 +0530

    Add files via upload

---
+++ stringMethods.java
@@ -0,0 +1,56 @@
+import java.util.Locale;
+
+public class stringMethods {
+    public static void main(String[] args) {
+      //  String a = "Mitu";
+       // String a1 = new String("Irfan");
+       // System.out.println(a1);
+        String name = "Mittu";
+      //  int value = name.length();   // Prints the length of the string
+       // System.out.println(value);
+       // String lstring = name.toLowerCase();   // Converts string into lower case
+      //  System.out.println(lstring);
+       // String ustring = name.toUpperCase();    // Converts string into upper case
+      // System.out.println(ustring);
+       // String nonTrimmedString = "     Mittu     ";
+      //  System.out.println("Non trimmed string is" +  nonTrimmedString);
+       // String trimmedString = nonTrimmedString.trim();    // Removes white spaces from the string
+       // System.out.println("Trimmed string is" + trimmedString);
+       // String sstring = name.substring(2,5);   // Prints the string from the index we mentioned to end of the string
+      //   System.out.println(sstring);
+      //  String rstring = name.replace('I','R');  // Replace the string with the given characters
+      //  System.out.println(rstring);
+       // System.out.println(name.replace('I', 'R'));   // Same as above replace code
+       // boolean value = name.startsWith("Mi");
+        /*
+          It is a boolean value
+          because if it starts
+          with the given string
+          it returns true else false
+        */
+       // System.out.println(value);
+       // boolean value = name.endsWith("tu");
+        /*
+        if it ends
+          with the given string
+          it returns true else false
+         */
+       // System.out.println(value);
+      //  System.out.println(name.charAt(2));   // Returns the character at the given index
+       // System.out.println(name.indexOf("t"));   // Returns the index of the given character at first occurrence
+        // System.out.println(name.lastIndexOf("t"));  // Returns the index of the given character at last occurrence
+       // System.out.println(name.equals("Mittu"));
+        /*
+        Returns true if the given
+        string is equal to the
+        string and it is case sensitive
+         */
+       // System.out.println(name.equalsIgnoreCase("mittu"));
+          /*
+        Returns true if the given
+        string is equal to the
+        string and this method is
+        not case sensitive
+         */
+    }
+}
